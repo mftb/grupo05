297dabd6ab5700e86e13f769e41cbb8b47427ade
Douglas Costa <douglasascosta@gmail.com>
2014-11-21 23:01:55 -0200

    voltando commit pro upload funcionar

--- caelum_VRaptor-master/src/br/unicamp/ic/mc437/grupo05/controller/FileController.java
+++ caelum_VRaptor-master/src/br/unicamp/ic/mc437/grupo05/controller/FileController.java
@@ -1,11 +1,9 @@
 package br.unicamp.ic.mc437.grupo05.controller;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.List;
+import java.io.StringWriter;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
@@ -14,26 +12,18 @@ import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.Validator;
 import br.com.caelum.vraptor.interceptor.multipart.UploadedFile;
 import br.unicamp.ic.mc437.grupo05.infra.XmlFileDao;
-import br.unicamp.ic.mc437.grupo05.modelo.TestFile;
 import br.unicamp.ic.mc437.grupo05.modelo.XmlFile;
-import br.unicamp.ic.mc437.grupo05.process.FileProcess;
-
-import com.google.common.io.Files;
 
 @Resource
 @Path("/file")
 public class FileController {
 
 	private XmlFileDao xmlFileDao;
-	private Result result;
-	private Validator validator;
 
 	public FileController(XmlFileDao xmlFileDao, Result result,
 			Validator validator) {
 		super();
 		this.xmlFileDao = xmlFileDao;
-		this.result = result;
-		this.validator = validator;
 	}
 
 	@Path("/list")
@@ -54,21 +44,12 @@ public class FileController {
 				return;
 			}
 
-			File textFile = new File(System.getProperty("user.dir"),
-					file.getFileName());
-			FileUtils.copyInputStreamToFile(file.getFile(), textFile);
-
-			List<String> lines = Files.readLines(textFile,
-					Charset.defaultCharset());
-			StringBuffer buffer = new StringBuffer();
-			for (String line : lines) {
-				buffer.append(line);
-			}
+			StringWriter writer = new StringWriter();
+			IOUtils.copy(file.getFile(), writer);
 
 			XmlFile xmlFile = new XmlFile();
 			xmlFile.setName(file.getFileName());
-
-			xmlFile.setContent(buffer.toString());
+			xmlFile.setContent(writer.toString());
 
 			xmlFileDao.salva(xmlFile);
 			System.out.println("Salvo!");
@@ -80,10 +61,11 @@ public class FileController {
 
 	@Path("/view")
 	public void view() {
-		String user = "TestUser";
-		FileProcess fileProcess = new FileProcess();
-		List<TestFile> sharedFilesList = fileProcess.getFilesSharedWith(user);
-		List<TestFile> ownedFilesList = fileProcess.getFilesOwnedBy(user);
+		// String user = "TestUser";
+		// FileProcess fileProcess = new FileProcess();
+		// List<TestFile> sharedFilesList =
+		// fileProcess.getFilesSharedWith(user);
+		// List<TestFile> ownedFilesList = fileProcess.getFilesOwnedBy(user);
 
 		// model.addAttribute("username", user);
 		// model.addAttribute("sharedFilesList", sharedFilesList);
